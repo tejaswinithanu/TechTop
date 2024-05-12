8a8b446c8f9a24a9af61fa492d67e035bc3444b2
Tejaswini <tejaswinithanubotla@gmail.com>
2024-05-12 15:17:21 +0000

    commit 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import {Switch, Route} from 'react-router-dom'
 
 import './App.css'
 import LoginPage from './components/LoginPage'
+import LandingPage from './components/LandingPage'
 
-const App = () => <LoginPage />
+const App = () => <LandingPage />
 
 export default App
---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,48 @@
+import Header from '../Header'
+
+import './index.css'
+
+const text = {
+  scrollText:
+    "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.The trouble with programmers is that you can never tell what a programmer is doing until it's too late.",
+}
+
+const LandingPage = () => (
+  <>
+    <Header />
+    <div className="video-container">
+      <video
+        className="bg-video"
+        src="https://res.cloudinary.com/dqqijdyjr/video/upload/v1715518208/aitglughdnnuidzbrrep.mp4"
+        autoPlay
+        loop
+        playsInLine
+        muted
+      >
+        <track kind="captions" />
+      </video>
+      <div className="text-bar">
+        <p className="text-inside-bar">{text.scrollText}</p>
+      </div>
+      <footer className="banner-container">
+        <p className="banner-text">
+          In computer programming, readability refers to the ease with which a
+          human reader can comprehend the purpose, control flow, and operation
+          of source code. It affects the aspects of quality above, including
+          portability, usability and most importantly maintainability.
+          Readability is important because programmers spend the majority of
+          their time reading, trying to understand, reusing, and modifying
+          existing source code, rather than writing new source code. Unreadable
+          code often leads to bugs, inefficiencies, and duplicated code. A study
+          found that a few simple readability transformations made code shorter
+          and drastically reduced the time to understand it.
+        </p>
+        <button className="explore-button" type="button">
+          Explore Tests
+        </button>
+      </footer>
+    </div>
+  </>
+)
+
+export default LandingPage
